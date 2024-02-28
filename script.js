634569aadd25b12b898359589fbef99d1397825d
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-02-28 18:32:28 -0500

    updated the js script

--- script.js
+++ script.js
@@ -89,14 +89,38 @@ const playSong = (id) => {
   const song = userData?.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  audio.play();
 };
+
+const pauseSong = () => {
+  userData.songCurrentTime = audio.currentTime;
+
+  playButton.classList.remove("playing");
+  audio.pause();
+};
+
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  }
+
+}
 
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song)=> {
       return `
       <li id="song-${song.id}" class="playlist-song">
-      <button class="playlist-song-info">
+      <button class="playlist-song-info" onclick="playSong(${song.id})">
           <span class="playlist-song-title">${song.title}</span>
           <span class="playlist-song-artist">${song.artist}</span>
           <span class="playlist-song-duration">${song.duration}</span>
@@ -113,16 +137,32 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
-userData?.songs.sort((a,b) => {
-  if (a.title < b.title) {
-    return -1;
-  }
+const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
-  if (a.title > b.title) {
-    return 1;
+playButton.addEventListener("click", () => {
+    if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    playSong(userData?.currentSong.id);
   }
-
-  return 0;
 });
 
-renderSongs(userData?.songs);
+pauseButton.addEventListener("click",  pauseSong);
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
