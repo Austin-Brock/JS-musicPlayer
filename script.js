e92241b638e26a27274c720883f34b467ba33747
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-03-05 13:54:12 -0500

    added new function setPlayButtonAccesibleText

--- script.js
+++ script.js
@@ -177,6 +177,10 @@ const renderSongs = (array) => {
   playlistSongs.innerHTML = songsHTML;
 };
 
+const setPlayButtonAccessibleText = () => {
+  
+}
+
 const getCurrentSongIndex = () => userData?.songs.indexOf(userData?.currentSong);
 
 playButton.addEventListener("click", () => {
