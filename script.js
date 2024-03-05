04ebbe4671030cc22ea2cc16f133836ecc035cf9
AustinBrockUGA <135121147+Austin-Brock@users.noreply.github.com>
2024-03-05 14:43:49 -0500

    updated deleteSong function

--- script.js
+++ script.js
@@ -158,7 +158,7 @@ const deleteSong = (id) => {
   highlightCurrentSong(); 
   setPlayButtonAccessibleText();
   if(userData?.songs.length === 0 ) {
-    
+    const resetButton = document.createElement('button');
   }
 };
 
